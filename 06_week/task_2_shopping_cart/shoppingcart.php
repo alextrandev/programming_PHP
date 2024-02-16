48beb220cfb7cb55affd1ed22743aea32c951d38
dk10119 <tranduclong092@gmail.com>
2024-02-16 19:15:07 +0200

    shopping cart task 2 completed

--- 06_week/task_2_shopping_cart/shoppingcart.php
+++ 06_week/task_2_shopping_cart/shoppingcart.php
@@ -1,17 +1,41 @@
 <?php
     // this is the associative array for our shopping cart 
     $cart = [];
-
+    $errormsg = "";
     // check if the cookie for shopping cart has been set (you can choose the name)
     // if it's been set, overwrite $cart variable with it. The easiest way to switch between array and string is to use JSON.
     // so e.g. $cart = json_decode($_COOKIE["cart"], true);
+    if(isset($_COOKIE["cart"])) {
+    $cart = json_decode($_COOKIE["cart"], true);
+}
 
-    // check if the add form has been sent
-    // if yes, add the new product to the $cart array and send the updated cookie
-    // let's use JSON again, so e.g. $json = json_encode($cart);
+// check if the add form has been sent
+// if yes, add the new product to the $cart array and send the updated cookie
+// let's use JSON again, so e.g. $json = json_encode($cart);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    @$productName = $_POST["productname"];
+    @$productAmount = $_POST["productamount"];
+    if (@$productAmount == "") {
+        $errormsg = "Please choose an amount!";
+    } else {
+        $errormsg = "";
+    }
+    for ($i = 0; $i < $productAmount; $i++) {
+        $cart[] = $productName;
+    }
+
+    $jsonCart = json_encode($cart);
+    setcookie("cart", $jsonCart);
 
     // check if the empty cart button has been pressed (key "emptycart" - the button's name attribute - exists in $_POST array)
     // if yes, empty the $cart array and set a new cookie that's set to expire in the past
+    if (@$_POST["emptycart"]) {
+        setcookie("cart", "", time() -60);
+        $cart = [];
+        $errormsg = "";
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,16 +46,22 @@
 </head>
 <body>
     <form action="shoppingcart.php" method="post">
-        <p>Choose product (choosing the same product will overwrite old entry):</p>
-        <p><label for="productname">Product name:</label><br>
+        <p>Choose product:</p>
+        <!-- <p><label for="productname">Product name:</label><br>
             <select id="productname" name="productname">
                 <option value="Book">Book</option>
                 <option value="Computer">Computer</option>
                 <option value="Phone">Phone</option>
             </select>
+        </p> -->
+        <p>
+            <input type="radio" id="book" name="productname" value="Book" checked ><label for="book">Book</label>
+            <input type="radio" id="computer" name="productname" value="Computer" <?php echo (@$productName == "Computer") ? 'checked' : ''; ?>><label for="computer">Computer</label>
+            <input type="radio" id="phone" name="productname" value="Phone" <?php echo (@$productName == "Phone") ? 'checked' : ''; ?>><label for="phone">Phone</label>
         </p>
         <p><label for="productamount">Amount:</label><br>
-            <input type="text" id="productamount" name="productamount">
+            <input type="number" id="productamount" name="productamount"><br>
+        <?php echo $errormsg ?>
         </p>
         <p><input type="submit" name="addproduct" value="Add to cart"></p>
     </form>
@@ -39,15 +69,24 @@
     <h3>Current shopping cart</h3>
     <div id="cart">
     <?php
-        // print the contents of the cart here (loop through the $cart array's key - value pairs)
-        // print e.g. 1 x Book
 
-        // Here's a fun little function: empty($cart) will tell you if the $cart array is empty
-        // if you check for emptiness before looping, then you could print e.g. "No items." if there's nothing in the cart
+// print the contents of the cart here (loop through the $cart array's key - value pairs)
+// print e.g. 1 x Book
+// Here's a fun little function: empty($cart) will tell you if the $cart array is empty
+// if you check for emptiness before looping, then you could print e.g. "No items." if there's nothing in the cart
+    $items = array_count_values($cart);
+    if (empty($cart)) {
+        echo 'Cart is empty!';
+    } else {
+        foreach ($items as $item => $amount) {
+            echo "$amount x $item<br>";
+        }
+    }
+
     ?>
 
     <form action="shoppingcart.php" method="post">
-        <input type="submit" name="emptycart" value="Empty cart">
+     <p><input type="submit" name="emptycart" value="Empty cart"></p>
     </form>
     </div>
 </body>
